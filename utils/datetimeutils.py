2ee5eba1ee02a79e8eccfdd5edd223b25b2ac54a
amritasaha87 <60414697+amritasaha87@users.noreply.github.com>
2020-02-19 10:52:58 +0800

    fixed date time parsing

--- utils/datetimeutils.py
+++ utils/datetimeutils.py
@@ -15,12 +15,17 @@ def extract_custom_date(s):
     s = s.lower()
     date = None
     if 'century' in s:
-        matches = re.findall("(\d+)", s)
-        for match in matches:
-            year = int(match)*100
+        num = extract_custom_numbers(s)
+        if num !=-1:
+            year = int(num)*100
             day = 1
             month = 1
-            break
+        else:    
+            matches = re.findall("(\d+)", s)
+            if len(matches)>0:
+                year = int(matches[0])*100
+                day = 1
+                month = 1
         date = (day, month, year)
     if 'week' in s:
         nums = get_number_from_string(s)
@@ -50,26 +55,22 @@ def extract_custom_date(s):
         if year==-1:
             year=1
         date = (1, 1, year)
-    else:
-        nums = get_number_from_string(s)
-        if nums!=-1:
-            day = nums
-            if day > 31:
-                day = int(day%31)
-                month = int(day/31)
-            else:
-                month = 1    
-            year = 0
-            date = (day, month, year)
     return date                
 
 
 
 def extract_custom_numbers(s):
+    print ('in extract custom numbers')
     ordinal_numbers = []
     for w in s.split(' '):
         if w in words_to_ordinal:
             ordinal_numbers.append(words_to_ordinal[w])
+        elif w.endswith('th') or w.endswith('st'):
+            try:
+                n = w2n.word_to_num(str(token)[:-2])
+                ordinal_numbers.append(n)
+            except:
+                continue       
     frac_numbers = []
     for w in s.split(' '):
         if w in words_to_frac:
@@ -82,27 +83,39 @@ def extract_custom_numbers(s):
         
 
 def get_date_from_string(s):
-        dates = []
-        date  = extract_custom_date(s)
+    dates = []
+    date  = extract_custom_date(s)
+    if date is not None:
         dates.append(date)
-        if not date:
-            try:
-                for dt in datefinder.find_dates(s):
-                    dates.append((dt.date().day, dt.date().month, dt.date().year))
-            except:
-                dates = []        
-            if len(dates)==0:
-                num = extract_custom_numbers(s)
+    else:
+        try:
+            for dt in datefinder.find_dates(s):
+                dates.append((dt.date().day, dt.date().month, dt.date().year))
+        except:
+            dates = []        
+        if len(dates)==0:
+            num = extract_custom_numbers(s)
+            if num!=-1:
+                date = (num, 1, 0)
+                print ('found custom DATE for', s, '->', date)
+            else:  
+                num = get_number_from_string(s)
                 if num!=-1:
-                    date = (num, 1, 0)
-                    print ('found custom DATE for', s, '->', date)
-                else:  
+                    day = num
+                    if day > 31:
+                        day = int(day%31)
+                        month = int(day/31)
+                    else:
+                        month = 1    
+                    year = 0
+                    date = (day, month, year)
+                else:    
                     print ('found no DATE for ', s) 
                     date = (-1, -1, -1)  
             dates = [date]            
-        else:
-            print ('found custom DATE for ', s, '->', dates[0])  
-        return dates[0]    
+    if len(dates)>0:
+        print ('found custom DATE for ', s, '->', dates[0])  
+    return dates[0]    
 
 def get_number_from_string(s):
         try:
@@ -124,6 +137,6 @@ def get_number_from_string(s):
                     nums = nums[0]
                 else:
                     nums = extract_custom_numbers(s)
-                    print ('found no NUM for ', s)
-                    nums = -1    
+                    if nums==-1:
+                        print ('found no NUM for ', s)
                 return nums    
